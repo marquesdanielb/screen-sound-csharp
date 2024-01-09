591e8c4fbea45fe84e1c727ed5ee9f8715b852bd
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2024-01-09 13:19:58 -0300

    Utiliza Linq para filtros e ordenacao

---
+++ Filtros/LinqOrder.cs
@@ -0,0 +1,19 @@
+﻿using ScreenSound_04.Modelos;
+
+namespace ScreenSound_04.Filtros;
+
+internal class LinqOrder
+{
+    public static void ExibirListaDeArtistaOrdenados(List<Musica> musica)
+    {
+        var artistaOrdenados = musica.OrderBy(musica 
+            => musica.Artista).Select(musica 
+            => musica.Artista).Distinct().ToList();
+        
+        Console.WriteLine("Lista de artistas Ordenados");
+        foreach (var artista in artistaOrdenados)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using ScreenSound_04.Modelos;
+using ScreenSound_04.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        LinqOrder.ExibirListaDeArtistaOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Professor Green");
+
+        //var musicasPreferidasDoMarcinho = new MusicasPreferidas("Marcinho");
+        //musicasPreferidasDoMarcinho.AdicionarMusicasFavoritas(musicas[7]);
+        //musicasPreferidasDoMarcinho.AdicionarMusicasFavoritas(musicas[284]);
+        //musicasPreferidasDoMarcinho.AdicionarMusicasFavoritas(musicas[117]);
+        //musicasPreferidasDoMarcinho.AdicionarMusicasFavoritas(musicas[6]);
+        //musicasPreferidasDoMarcinho.AdicionarMusicasFavoritas(musicas[1462]);
+        
+        //musicasPreferidasDoMarcinho.ExibirMusicasFavoritas();
+        //musicasPreferidasDoMarcinho.GerarArquivoJson();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
